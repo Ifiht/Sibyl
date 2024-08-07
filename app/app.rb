6d716ca5a4dd0d3935c7deb4a621b0930d49d25d
Peter Pan <peter@never.lan>
2024-08-07 18:45:30 -0400

    format and fix ubuntu deps

--- app/app.rb
+++ app/app.rb
@@ -1,5 +1,5 @@
-require 'tk'
-require 'sibyl'
+require "tk"
+require "sibyl"
 
 # Declare the Tkinter window root:
 root = TkRoot.new { title "Sibyl" }
@@ -7,8 +7,9 @@ root = TkRoot.new { title "Sibyl" }
 #------------//  TRIGGER FUNCTIONS  //----------------#
 #=====================================================#
 def exit_app(main_window)
-   main_window.destroy
+  main_window.destroy
 end
+
 #=====================================================#
 #------------//  ROOT SIZE & PLACEMENT  //------------#
 #=====================================================#
@@ -16,8 +17,8 @@ window_x = 500
 window_y = 500
 screen_width = root.winfo_screenwidth()
 screen_height = root.winfo_screenheight()
-center_x = (screen_width/2 - window_x/2)
-center_y = (screen_height/2 - window_y/2)
+center_x = (screen_width / 2 - window_x / 2)
+center_y = (screen_height / 2 - window_y / 2)
 root.geometry("#{window_x}x#{window_y}+#{center_x}+#{center_y}")
 root.resizable(0, 0)
 #=====================================================#
@@ -26,14 +27,14 @@ root.resizable(0, 0)
 menubar = TkMenu.new(root)
 filemenu = TkMenu.new(menubar)
 filemenu.tearoff = false
-menubar.add :cascade, :menu => filemenu, :label => 'File'
-filemenu.add :command, :label => 'Exit', :command => proc{exit_app(root)}
-filemenu.add :command, :label => 'Say Hello', :command => proc{puts "hello"}
+menubar.add :cascade, :menu => filemenu, :label => "File"
+filemenu.add :command, :label => "Exit", :command => proc { exit_app(root) }
+filemenu.add :command, :label => "Say Hello", :command => proc { puts "hello" }
 root.menu(menubar)
 #=====================================================#
 #------------//  ROOT GRAPHICS  //--------------------#
 #=====================================================#
-icon = TkPhotoImage.new('file' => 'crystal.gif')
+icon = TkPhotoImage.new("file" => "crystal.gif")
 root.iconphoto(icon)
 logo = TkPhotoImage.new
 logo.file = "sibyl.gif"
@@ -41,44 +42,41 @@ logo.file = "sibyl.gif"
 #------------//  ROOT LAYOUT  //----------------------#
 #=====================================================#
 sin_frame = TkFrame.new(root) { # Left Frame
-   grid('row'=>0, 'column'=>0)#, 'rowspan'=>3)
-   width 200
+  grid("row" => 0, "column" => 0) #, 'rowspan'=>3)
+  width 200
 }
 dex_frame = TkFrame.new(root) { # Right Frame
-   grid('row'=>0, 'column'=>1)#, 'rowspan'=>3)
-   width 300
+  grid("row" => 0, "column" => 1) #, 'rowspan'=>3)
+  width 300
 }
 TkLabel.new(sin_frame) {
-   text 'Contacts:'
-   grid('row'=>0, 'column'=>0, 'sticky'=>'se')
+  text "Contacts:"
+  grid("row" => 0, "column" => 0, "sticky" => "se")
 }
 TkLabel.new(dex_frame) {
-   text 'COL2'
-   grid('row'=>0, 'column'=>1)
+  text "COL2"
+  grid("row" => 0, "column" => 1)
 }
 TkLabel.new(dex_frame) {
-   text 'COL3'
-   grid('row'=>0, 'column'=>2)
+  text "COL3"
+  grid("row" => 0, "column" => 2)
 }
 TkLabel.new(dex_frame) {
-   text 'COL4'
-   grid('row'=>0, 'column'=>3)
+  text "COL4"
+  grid("row" => 0, "column" => 3)
 }
 
 listbox = TkListbox.new(sin_frame) {
-   height 19
-   selectmode 'single'
-   grid('row'=>1, 'column'=>0, 'rowspan'=>4)
+  height 19
+  selectmode "single"
+  grid("row" => 1, "column" => 0, "rowspan" => 4)
 }
 listbox.insert 0, "one", "two", "three"
 TkLabel.new(sin_frame) {
-   image logo
-   grid('row'=>0, 'column'=>0, 'sticky'=>'sw')
+  image logo
+  grid("row" => 0, "column" => 0, "sticky" => "sw")
 }
 #=====================================================#
 #------------//  RUN THE APP  //----------------------#
 #=====================================================#
 Tk.mainloop
-
-
-
--- lib/sibyl/persona.rb
+++ lib/sibyl/persona.rb
@@ -1,8 +1,8 @@
 class Persona
   attr_accessor(:given_name, :family_name, :addtnl_names, :honor_prefxs, :honor_suffxs, :socials)
 
-  def initialize(given_name, family_name = nil, addtnl_names = [], honor_prefxs = [], 
-    honor_suffxs = [], socials = [])
+  def initialize(given_name, family_name = nil, addtnl_names = [], honor_prefxs = [],
+                             honor_suffxs = [], socials = [])
     @given_name = given_name
     @family_name = family_name
     @addtnl_names = addtnl_names
